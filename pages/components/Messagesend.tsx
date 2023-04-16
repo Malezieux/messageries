6b41d530f08501016d86263d72113d240bb94234
color.branch <philippe.malezieux@afnor.org>
2023-04-16 17:02:09 +0200

    conserve affichage des message

--- pages/components/Messagesend.tsx
+++ pages/components/Messagesend.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 export default function UserForm() {
   const { register, handleSubmit, watch } = useForm();
   const [message, setMessage] = useState('');
+  const [messagesList, setMessagesList] = useState<string[]>([]); // Etat (state) de messages
+
   const onSubmit = async () => {
     const data = watch(); 
     const response = await fetch("http://localhost:8000/api/eleve", {
@@ -13,24 +15,28 @@ export default function UserForm() {
       },
       body: JSON.stringify({
         message_send: data.message_send,
-       
       }),
     });
     const jsonResponse = await response.json();
     setMessage(jsonResponse.message);
+
+    // Ajouter le nouveau message à l'état (state) de messages
+    setMessagesList([...messagesList, jsonResponse.message]);
   };
 
   return (
-  <>
-     {message && <p>{message}</p>} 
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>
-        Message :
-        <input {...register("message_send")} />
-      </label>
-     
-      <button type="submit">Envoyer</button>
+    <>
+      {message && <p>{message}</p>}
+      {messagesList.map((message, index) => (
+        <p key={index}>{message}</p>
+      ))}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Message de test :
+          <input {...register("message_send")} />
+        </label>
+        <button type="submit">Envoyer</button>
       </form>
-      </>
+    </>
   );
 }
